70e1ce433c03c4697ba9f36dac7cf8c6f2bf6ba1
dimagolomozy <dima.golomozy@gmail.com>
2026-05-05 15:30:33 +0300

    fixes

--- examples/datafy-iam-role-global/main.tf
+++ examples/datafy-iam-role-global/main.tf
@@ -11,12 +11,11 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "development"
 }
 
 module "datafy_role" {
-  source  = "../../"
-  # version = "~> 1.0"
+  source  = "datafy-io/iam-role/datafy"
+  version = "~> 1.0"
 
   account_id        = "123e4567-e89b-12d3-a456-426614174000"
   permissions_level = "AutoScaler"
